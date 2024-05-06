79012be42bb7513e31a87f4944d7c197fb2881cb
SergioLM7 <164863757+SergioLM7@users.noreply.github.com>
2024-05-06 17:35:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+//Parte inicial
+
+//1.Variable con las balas
 const cargador = ['pium!', 'pium!', 'pium!', 'pium!', 'pium!', 'pium!', 'pium!'];
 
+//2.Función que imprima los disparos
 const chauchat = (argumento) => {
     console.log(argumento);
 };
 
+//Parte Premium
+//3. y 4. 80% de probabilidad de que se atasque y NO dispare; cada 3 disparos, imprimir un espacio
 let pillada = Math.random();
 
 const evitarCalentar = (argumento) => {
     for (let i = 0; i < argumento.length; i++) {
         console.log(argumento[i]);
-        if (i === 2 || i === 5 || i === 8) {
+        if ((i+1) % 3 === 0) {
             console.log('');
         }
     }
 };
 
-if (pillada < 0.8) {
+if (pillada <= 0.8) {
     console.log('Illo, me he quedao pillá!');   
 } else {
     evitarCalentar(cargador);
